83bb5bb26581eb30b183e6bd056a21dfbe62e792
juliogreff <juliogreff@gmail.com>
2019-12-03 10:59:23 +0100

    Report capacity progression through a Ready condition
    We're moving towards relocating the smarts in the checks for a target's
    readiness away from the executor and into the controllers themselves,
    leaving the executor to check for a single `Ready` condition at the
    target's top level status. This commit does exactly that for
    CapacityTarget objects.

--- pkg/controller/traffic/traffic_controller_test.go
+++ pkg/controller/traffic/traffic_controller_test.go
@@ -244,7 +244,7 @@ func runTrafficControllerTest(
 	objectsByCluster map[string][]runtime.Object,
 	expectations []trafficTargetTestExpectation,
 ) {
-	f := NewControllerTestFixture()
+	f := shippertesting.NewControllerTestFixture()
 
 	clusterNames := []string{}
 	for clusterName, objects := range objectsByCluster {
@@ -357,7 +357,7 @@ func assertPodTraffic(
 	}
 }
 
-func runController(f *ControllerTestFixture) {
+func runController(f *shippertesting.ControllerTestFixture) {
 	controller := NewController(
 		f.ShipperClient,
 		f.ShipperInformerFactory,
