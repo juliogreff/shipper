cebf072ef1684d2a2bfa2d6edad202289a5df8cd
juliogreff <juliogreff@gmail.com>
2020-01-10 10:16:38 +0100

    Testing github actions

---
+++ ci/publish_images.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -ex
+
+docker tag $DOCKER_REGISTRY/shipper:$IMAGE_TAG juliogreff/shipper:$IMAGE_TAG
+docker tag $DOCKER_REGISTRY/shipper:$IMAGE_TAG juliogreff/shipper:latest
+docker push juliogreff/shipper
+
+docker tag $DOCKER_REGISTRY/shipper-state-metrics:$IMAGE_TAG juliogreff/shipper-state-metrics:$IMAGE_TAG
+docker tag $DOCKER_REGISTRY/shipper-state-metrics:$IMAGE_TAG juliogreff/shipper-state-metrics:latest
+docker push juliogreff/shipper-state-metrics
